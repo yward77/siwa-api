a1ac54cb3aef982c52780fd8b52c7aa8df426ecc
yward77 <yazanwward756@gmail.com>
2026-04-15 02:00:46 +0200

    Switch to direct IPv4 connection

--- config/database.js
+++ config/database.js
@@ -3,7 +3,8 @@ module.exports = ({ env }) => ({
     client: 'postgres',
     connection: {
       connectionString: env('DATABASE_URL'),
-      ssl: env.bool('DATABASE_SSL', false) ? { rejectUnauthorized: false } : false,
+      ssl: { rejectUnauthorized: false },
     },
+    pool: { min: 2, max: 10 }
   },
 });
