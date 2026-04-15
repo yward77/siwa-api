b3bc1ac6ccd622d4fa68556e074a8c69c55a2e9a
yward77 <yazanwward756@gmail.com>
2026-04-15 15:53:40 +0200

    Final fix for Cloudinary image storage

--- config/plugins.js
+++ config/plugins.js
@@ -7,6 +7,10 @@ module.exports = ({ env }) => ({
         api_key: env('CLOUDINARY_KEY'),
         api_secret: env('CLOUDINARY_SECRET'),
       },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
     },
   },
 });
