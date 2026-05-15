920da6177bcf4c50b868822f26a6f4c752a08393
yward77 <yazanward756@gmail.com>
2026-05-15 14:45:08 +0300

    Fix: update database configuration to use Neon PostgreSQL

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      connectionString: env('DATABASE_URL'),
-      ssl: { rejectUnauthorized: false },
+      connectionString: env('DATABASE_URL'), // هنا يقرأ من متغير البيئة
+      // أو ربما تجد الرابط القديم مكتوباً هنا مباشرة (Hardcoded)!
     },
   },
 });
